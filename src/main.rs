7fa67fb147b6b3ec693c1f415d2bd6794f670110
ShawnYuan <ysh83737@hotmail.com>
2024-03-01 18:21:15 +0800

    35. 搜索插入位置 O(logn)

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 fn main() {
+    assert_eq!(Solution::search_insert(vec![1,3,5,6], 0), 0);
+    assert_eq!(Solution::search_insert(vec![1,3,5,6], 4), 2);
     assert_eq!(Solution::search_insert(vec![1,3,5,6], 5), 2);
+    assert_eq!(Solution::search_insert(vec![1,3,5,6], 6), 3);
+    assert_eq!(Solution::search_insert(vec![1,3,5,6], 7), 4);
     assert_eq!(Solution::search_insert(vec![1,3,5,6], 2), 1);
 }
 
 struct Solution {}
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        for index in 0..nums.len() {
-            let item = nums[index];
-            if item >= target {
-                return index as i32;
+        let mut left: usize = 0;
+        let mut right: usize = nums.len();
+        while right - left > 1 {
+            let middle = left + (((right - left) / 2) as f64).round() as usize;
+            let middle_num = nums[middle];
+
+            if middle_num == target {
+                return middle as i32;
+            } else if middle_num > target {
+                right = middle;
+            } else {
+                left = middle;
             }
         }
-        nums.len() as i32
+
+        if target > nums[left] {
+            return right as i32;
+        }
+        left as i32
     }
 }
