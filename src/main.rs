82c46ded8eb94e91a93544de7205af1b785579b0
ShawnYuan <ysh83737@hotmail.com>
2024-03-06 17:16:47 +0800

    121. 买卖股票的最佳时机

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ struct Solution {}
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let mut result = 0;
-        let mut min_price = i32::MAX;
-        for i in 0..prices.len() {
+        let mut min_price = prices[0];
+        for i in 1..prices.len() {
             let price = prices[i];
-            if price < min_price {
+            if price > prices[i - 1] {
+                result = result.max(price - min_price);
+            } else if price < min_price {
                 min_price = price;
-            } else if (price - min_price) > result {
-                result = price - min_price;
             }
         }
         result
