c749d923ed028973e2bbbc2bc4cf90c4d17e6d45
ShawnYuan <ysh83737@hotmail.com>
2024-03-06 16:33:00 +0800

    119. 杨辉三角 II O(n) 数学解法

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 fn main() {
-    println!("{:?}", Solution::get_row(3));
+    println!("{:?}", Solution::get_row(30));
 }
 
 struct Solution {}
 impl Solution {
-    pub fn get_row(row_index: i32) -> Vec<i32> {
-        let mut result: Vec<i32> = vec![1];
-        for row in 1..=(row_index as usize) {
-            result.push(1);
-            for idx in (1..row).rev() {
-                result[idx] += result[idx - 1];
-            }
-            println!("{:?}", result);
+    pub fn get_row(row_index: i32) -> Vec<i64> {
+        let row_index = row_index as i64;
+        let mut result: Vec<i64> = vec![0; (row_index + 1) as usize];
+        result[0] = 1;
+        for idx in 1..=(row_index) {
+            result[idx as usize] = result[(idx - 1) as usize] * (row_index - idx + 1) / idx;
         }
         result
     }
