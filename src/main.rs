34f1ee20d2dc5a985332fb3664b74e9ce21352e6
ShawnYuan <ysh83737@hotmail.com>
2024-03-08 17:57:57 +0800

    169. 多数元素 O(n), O(1) 不需要额外空间

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,16 @@ fn main() {
 struct Solution {}
 impl Solution {
     pub fn majority_element(nums: Vec<i32>) -> i32 {
-        use std::collections::HashMap;
-        let mut map: HashMap<i32, u32> = HashMap::new();
         let mut result = 0;
-        let mut max_count = 0;
+        let mut count = 0;
         for num in nums {
-            let count = map.entry(num).or_insert(0);
-            *count += 1;
-            if *count > max_count {
-                max_count = *count;
+            if count == 0 {
                 result = num;
+            }
+            if num == result {
+                count += 1;
+            } else {
+                count -= 1;
             }
         }
         result
