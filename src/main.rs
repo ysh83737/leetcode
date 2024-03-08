34ec301a657ae90d036ac2c545d1224b2328fc8c
ShawnYuan <ysh83737@hotmail.com>
2024-03-08 17:37:53 +0800

    136. 只出现一次的数字 O(n) 异或运算 编译元素

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ struct Solution {}
 impl Solution {
     pub fn single_number(nums: Vec<i32>) -> i32 {
         let mut result = 0;
-        for i in 0..nums.len() {
-            result ^= nums[i];
+        for num in nums {
+            result ^= num;
         }
         result
     }
