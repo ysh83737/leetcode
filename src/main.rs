71ec50383ce8b936b54126c2e72de56f98190bfc
ShawnYuan <ysh83737@hotmail.com>
2024-03-08 17:55:25 +0800

    169. 多数元素 O(n), O(n) 计数，需要额外空间

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 fn main() {
-    assert_eq!(Solution::single_number(vec![2,2,1]), 1);
+    assert_eq!(Solution::majority_element(vec![2,2,1,1,1,2,2]), 2);
 }
 
 struct Solution {}
 impl Solution {
-    pub fn single_number(nums: Vec<i32>) -> i32 {
+    pub fn majority_element(nums: Vec<i32>) -> i32 {
+        use std::collections::HashMap;
+        let mut map: HashMap<i32, u32> = HashMap::new();
         let mut result = 0;
+        let mut max_count = 0;
         for num in nums {
-            result ^= num;
+            let count = map.entry(num).or_insert(0);
+            *count += 1;
+            if *count > max_count {
+                max_count = *count;
+                result = num;
+            }
         }
         result
     }
