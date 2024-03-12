9fe7b1c91efdf95045b4dc1d0052621a356c2ca5
ShawnYuan <ysh83737@hotmail.com>
2024-03-12 18:20:59 +0800

    219. 存在重复元素 II O(n)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 fn main() {
-    assert_eq!(Solution::contains_duplicate(vec![1,2,3,1]), true);
-    assert_eq!(Solution::contains_duplicate(vec![1,1,1,3,3,4,3,2,4,2]), true);
+    assert_eq!(Solution::contains_nearby_duplicate(vec![1,2,3,1], 3), true);
 }
 
 struct Solution {}
 impl Solution {
-    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-        use std::collections::HashSet;
-        let mut set = HashSet::new();
-        for num in nums {
-            if set.contains(&num) {
-                return true;
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        use std::collections::HashMap;
+        let mut map = HashMap::new();
+        for i in 0..nums.len() {
+            let num = nums[i];
+            if let Some((_, j)) = map.get_key_value(&num) {
+                if i - j <= k as usize {
+                    return true;
+                }
             }
-            set.insert(num);
+            map.insert(num, i);
         }
         false
     }
