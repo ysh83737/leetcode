d8bd3e0c96045a934706b9ea766c37e6a3b663d8
ShawnYuan <ysh83737@hotmail.com>
2024-03-12 18:23:12 +0800

    219. 存在重复元素 II O(n) 语法优化

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     assert_eq!(Solution::contains_nearby_duplicate(vec![1,2,3,1], 3), true);
 }
@@ -5,16 +7,14 @@ fn main() {
 struct Solution {}
 impl Solution {
     pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
-        use std::collections::HashMap;
         let mut map = HashMap::new();
         for i in 0..nums.len() {
             let num = nums[i];
-            if let Some((_, j)) = map.get_key_value(&num) {
+            if let Some(j) = map.insert(num, i) {
                 if i - j <= k as usize {
                     return true;
                 }
             }
-            map.insert(num, i);
         }
         false
     }
