5f036144c8a1259f8682f1f2441d250ec9e51e4c
ShawnYuan <ysh83737@hotmail.com>
2024-03-13 10:15:42 +0800

    228. 汇总区间 O(n)

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
-use std::collections::HashMap;
-
 fn main() {
-    assert_eq!(Solution::contains_nearby_duplicate(vec![1,2,3,1], 3), true);
+    assert_eq!(Solution::summary_ranges(vec![] as Vec<i32>), vec![] as Vec<String>);
+    assert_eq!(Solution::summary_ranges(vec![0,1,2,4,5,7]), vec!["0->2","4->5","7"]);
+    assert_eq!(Solution::summary_ranges(vec![0,2,3,4,6,8,9]), vec!["0","2->4","6","8->9"]);
 }
 
 struct Solution {}
 impl Solution {
-    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
-        let mut map = HashMap::new();
-        for i in 0..nums.len() {
-            let num = nums[i];
-            if let Some(j) = map.insert(num, i) {
-                if i - j <= k as usize {
-                    return true;
-                }
-            }
+    pub fn summary_ranges(nums: Vec<i32>) -> Vec<String> {
+        let mut result = vec![];
+        if nums.len() == 0 {
+            return result;
         }
-        false
+        let mut start = nums[0];
+        let mut end = nums[0];
+        for i in 1..nums.len() {
+            let num = nums[i];
+            if num > end + 1 {
+                Solution::push_range(&mut result, start, end);
+                start = num;
+            }
+            end= num;
+        }
+        Solution::push_range(&mut result, start, end);
+        result
+    }
+    fn push_range(ranges: &mut Vec<String>, start: i32, end: i32) -> () {
+        if start == end {
+            ranges.push(format!("{}", start));
+        } else {
+            ranges.push(format!("{}->{}", start, end));
+        }
     }
 }
