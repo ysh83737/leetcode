e65b4cd7ec0d9e23bcc280d29a89f7c569171960
ShawnYuan <ysh83737@hotmail.com>
2024-03-15 16:29:02 +0800

    268. 丢失的数字 O(n) 数学计算

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,15 @@
 fn main() {
-    assert_eq!(Solution::summary_ranges(vec![] as Vec<i32>), vec![] as Vec<String>);
-    assert_eq!(Solution::summary_ranges(vec![0,1,2,4,5,7]), vec!["0->2","4->5","7"]);
-    assert_eq!(Solution::summary_ranges(vec![0,2,3,4,6,8,9]), vec!["0","2->4","6","8->9"]);
+    assert_eq!(Solution::missing_number(vec![3,0,1]), 2);
 }
 
 struct Solution {}
 impl Solution {
-    pub fn summary_ranges(nums: Vec<i32>) -> Vec<String> {
-        let len = nums.len();
-        let mut result = vec![];
-        let mut i = 0;
-        while i < len {
-            let start = nums[i];
-            while i < len - 1 && nums[i] + 1 == nums[i + 1] {
-                i += 1;
-            }
-            let end = nums[i];
-            if start == end {
-                result.push(format!("{}", start));
-            } else {
-                result.push(format!("{}->{}", start, end));
-            }
-            i += 1;
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        let n = nums.len() as i32;
+        let mut sum = 0;
+        for num in nums {
+            sum += num;
         }
-        result
+        n * (n + 1) / 2 - sum
     }
 }
