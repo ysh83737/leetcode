e58bf31dd96dfdd8636ea4d50967619982ba1aa5
ShawnYuan <ysh83737@hotmail.com>
2024-03-19 11:24:27 +0800

    350. 两个数组的交集 II O(n+m) HashMap计数

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,23 @@ fn main() {
 struct Solution;
 impl Solution {
     pub fn intersect(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        if nums1.len() > nums2.len() {
+            return  Solution::intersect(nums2, nums1);
+        }
         use std::collections::HashMap;
-        let mut map1: HashMap<i32, u32> = HashMap::new();
+        let mut map: HashMap<i32, u32> = HashMap::new();
         for num in nums1 {
-            map1.entry(num).and_modify(|count| { *count += 1; }).or_insert(1);
+            map.entry(num).and_modify(|count| { *count += 1; }).or_insert(1);
         }
-        let mut map2: HashMap<i32, u32> = HashMap::new();
+        let mut result = vec![];
         for num in nums2 {
-            if let Some(count1) = map1.get(&num) {
-                map2.entry(num).and_modify(|count2| {
-                    if *count2 < *count1 {
-                        *count2 += 1;
-                    }
-                }).or_insert(1);
-            }
+            map.entry(num).and_modify(|count| {
+                if *count > 0 {
+                    result.push(num);
+                    *count -= 1;
+                }
+            });
         }
-        map2.into_iter().flat_map(|(num, count)| vec![num; count as usize]).collect()
+        result
     }
 }
