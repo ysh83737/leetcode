f2afc9cbf361b86ec12befd437749027ec8c46cd
ShawnYuan <ysh83737@hotmail.com>
2024-03-19 10:16:15 +0800

    349. 两个数组的交集 O(n) HashSet

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
 fn main() {
-    let nums = vec![-2, 0, 3, -5, 2, -1];
-    let num_array = NumArray::new(nums);
-    assert_eq!(num_array.sum_range(0, 2), 1);
-    assert_eq!(num_array.sum_range(2, 5), -1);
-    assert_eq!(num_array.sum_range(0, 5), -3);
+    let nums1 = vec![1,2,2,1];
+    let nums2 = vec![2,2];
+    assert_eq!(Solution::intersection(nums1, nums2), vec![2]);
 }
 
-struct NumArray {
-    sums: Vec<i32>,
-}
-
-impl NumArray {
-    fn new(nums: Vec<i32>) -> Self {
-        let mut sums = vec![0];
-        for (i, num) in nums.iter().enumerate() {
-            sums.push(sums[i] + num)
+struct Solution;
+impl Solution {
+    pub fn intersection(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        use std::collections::HashSet;
+        let mut set = HashSet::new();
+        for num in nums1 {
+            set.insert(num);
         }
-        NumArray { sums }
-    }
-    fn sum_range(&self, left: i32, right: i32) -> i32 {
-        self.sums[(right + 1) as usize] - self.sums[left as usize]
+        let mut result = HashSet::new();
+        for num in nums2 {
+            if set.contains(&num) {
+                result.insert(num);
+            }
+        }
+        result.iter().map(|num| num * 1).collect()
     }
 }
