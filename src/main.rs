54de34cd9dbed453660ccea7581b49f5cd15c48a
ShawnYuan <ysh83737@hotmail.com>
2024-03-19 14:24:42 +0800

    414. 第三大的数 O(n) 暴力解法

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,51 @@
 fn main() {
-    let nums1 = vec![1,2,2,1];
-    let nums2 = vec![2,2];
-    assert_eq!(Solution::intersect(nums1,nums2), vec![2,2]);
+    assert_eq!(Solution::third_max(vec![3, 2, 1]), 1);
+    assert_eq!(Solution::third_max(vec![2, 1]), 2);
+    assert_eq!(Solution::third_max(vec![2, 2, 3, 1]), 1);
+    assert_eq!(Solution::third_max(vec![1,2,-2147483648]), -2147483648);
 }
 
 struct Solution;
 impl Solution {
-    pub fn intersect(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-        let mut nums1 = nums1.clone();
-        let mut nums2 = nums2.clone();
-        nums1.sort();
-        nums2.sort();
-        // 基于排序的双指针实现
-
-        let mut result = vec![];
-        let mut i = 0;
-        let mut j = 0;
-        while i < nums1.len() && j < nums2.len() {
-            let (n1, n2) = (nums1[i], nums2[j]);
-            if n1 == n2 {
-                result.push(n1);
-                i += 1;
-                j += 1;
-            } else if n1 > n2 {
-                j += 1;
-            } else {
-                i += 1;
+    pub fn third_max(nums: Vec<i32>) -> i32 {
+        let mut max1 = None;
+        let mut max2 = None;
+        let mut max3 = None;
+        for mut num in nums {
+            let o_num = Some(num);
+            if o_num == max1 || o_num == max2 || o_num == max3 {
+                continue;
+            }
+            if max1 == None {
+                max1 = Some(num);
+                continue;
+            } else if let Some(n) = max1 {
+                if num > n {
+                    max1 = Some(num);
+                    num = n;
+                }
+            }
+            if max2 == None {
+                max2 = Some(num);
+                continue;
+            } else if let Some(n) = max2 {
+                if num > n {
+                    max2 = Some(num);
+                    num = n;
+                }
+            }
+            if max3 == None {
+                max3 = Some(num);
+                continue;
+            } else if let Some(n) = max3 {
+                if num > n {
+                    max3 = Some(num);
+                }
             }
         }
-        result
+        if max3 == None {
+            return max1.unwrap();
+        }
+        max3.unwrap()
     }
 }
