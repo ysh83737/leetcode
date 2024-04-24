5893156d93204cb92c579f7b83f7ebffe9368ea4
ShawnYuan <ysh83737@hotmail.com>
2024-04-24 17:48:17 +0800

    495. 提莫攻击 O(n)

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
-use std::cmp::max;
-
 fn main() {
-    assert_eq!(Solution::find_max_consecutive_ones(vec![1,1,0,1,1,1]), 3);
-    assert_eq!(Solution::find_max_consecutive_ones(vec![1,1,0,0,0,0,0,0]), 2);
-    assert_eq!(Solution::find_max_consecutive_ones(vec![1,1,0,0,1,0]), 2);
-    assert_eq!(Solution::find_max_consecutive_ones(vec![1,0,0,0]), 1);
-    assert_eq!(Solution::find_max_consecutive_ones(vec![0,0,1,0,0,0]), 1);
-    assert_eq!(Solution::find_max_consecutive_ones(vec![0,0,0,0]), 0);
+    assert_eq!(Solution::find_poisoned_duration(vec![1,4], 2), 4);
+    assert_eq!(Solution::find_poisoned_duration(vec![1,2], 2), 3);
 }
 
 struct Solution;
 
 impl Solution {
-    pub fn find_max_consecutive_ones(nums: Vec<i32>) -> i32 {
+    pub fn find_poisoned_duration(time_series: Vec<i32>, duration: i32) -> i32 {
         let mut result = 0;
-        let mut temp = 0;
-        for num in nums {
-            if num == 1 {
-                temp += 1;
-            } else {
-                temp = 0;
+        // 中毒结束
+        let mut end = 0;
+        for t in time_series {
+            result += duration;
+            if t < end {
+                result -= end - t;
             }
-            result = max(result, temp);
+            end = t + duration
         }
         result
     }
