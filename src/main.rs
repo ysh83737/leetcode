2d0c8000b8182997129a7384187a5fe01bce1802
ShawnYuan <ysh83737@hotmail.com>
2024-05-08 15:57:22 +0800

    496. 下一个更大元素 I 暴力解法

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,41 @@
+use std::collections::HashMap;
+
 fn main() {
-    assert_eq!(Solution::find_poisoned_duration(vec![1,4], 2), 4);
-    assert_eq!(Solution::find_poisoned_duration(vec![1,2], 2), 3);
+    assert_eq!(Solution::next_greater_element(vec![4,1,2], vec![1,3,4,2]), vec![-1,3,-1]);
+    assert_eq!(Solution::next_greater_element(vec![2,4], vec![1,2,3,4]), vec![3, -1]);
 }
 
 struct Solution;
 
 impl Solution {
-    pub fn find_poisoned_duration(time_series: Vec<i32>, duration: i32) -> i32 {
-        let mut result = 0;
-        // 中毒结束
-        let mut end = 0;
-        for t in time_series {
-            result += duration;
-            if t < end {
-                result -= end - t;
-            }
-            end = t + duration
+    pub fn next_greater_element(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
+        let mut ans = vec![];
+
+        let len2 = nums2.len();
+
+        let mut nums2_map = HashMap::new();
+        for i in 0..len2 {
+            let num = nums2[i];
+            nums2_map.insert(num, i);
         }
-        result
+
+        let greater = |mut i: usize| {
+            let num = nums2[i];
+            i += 1;
+            while i < len2 {
+                let n = nums2[i];
+                if n > num {
+                    return n;
+                }
+                i += 1;
+            }
+            -1
+        };
+        for num in nums1 {
+            let i = nums2_map.get(&num).cloned().unwrap();
+            ans.push(greater(i));
+        }
+
+        ans
     }
 }
