3e557ac1effa5f89983ef2e44e24c4431e481ca5
ShawnYuan <ysh83737@hotmail.com>
2024-05-08 16:31:17 +0800

    496. 下一个更大元素 I 单调栈 O(m + n) O(n)

--- src/main.rs
+++ src/main.rs
@@ -9,33 +9,40 @@ struct Solution;
 
 impl Solution {
     pub fn next_greater_element(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-        let mut ans = vec![];
+        // nums2中所有值对应的答案Map
+        let mut ans_map = HashMap::new();
 
-        let len2 = nums2.len();
-
-        let mut nums2_map = HashMap::new();
-        for i in 0..len2 {
-            let num = nums2[i];
-            nums2_map.insert(num, i);
-        }
-
-        let greater = |mut i: usize| {
-            let num = nums2[i];
-            i += 1;
-            while i < len2 {
-                let n = nums2[i];
-                if n > num {
-                    return n;
+        // 单调栈
+        let mut stack = vec![];
+        let mut i = nums2.len();
+        while i > 0 {
+            let num = nums2[i - 1];
+            let mut j = stack.len();
+            while j > 0 {
+                let s_num = stack[j - 1];
+                if s_num > num {
+                    // 入栈
+                    stack.push(num);
+                    // 记录答案
+                    ans_map.insert(num, s_num);
+                    break;
+                } else {
+                    stack.pop();
                 }
-                i += 1;
+                j -= 1;
             }
-            -1
-        };
-        for num in nums1 {
-            let i = nums2_map.get(&num).cloned().unwrap();
-            ans.push(greater(i));
+            if j == 0 {
+                // 空栈 入栈
+                stack.push(num);
+            }
+            i -= 1;
         }
 
-        ans
+        let mut result = vec![];
+        for num in nums1 {
+            let ans = ans_map.get(&num).unwrap_or(&-1);
+            result.push(*ans);
+        }
+        result
     }
 }
