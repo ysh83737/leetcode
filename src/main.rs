1fc1d2cd01c88679ee079a507ec24d6518e3e3ff
ShawnYuan <ysh83737@hotmail.com>
2024-05-09 15:38:00 +0800

    503. 下一个更大元素 II 正向遍历的单调栈 O(2n) O(n)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::VecDeque;
 
 fn main() {
     assert_eq!(Solution::next_greater_elements(vec![1,2,1]), vec![2,-1,2]);
@@ -10,40 +10,20 @@ struct Solution;
 
 impl Solution {
     pub fn next_greater_elements(nums: Vec<i32>) -> Vec<i32> {
-        // nums中所有索引对应的答案Map
-        let mut ans_map = HashMap::new();
-
-        // 单调栈
-        let mut stack = vec![];
         let n = nums.len();
-        let mut i = n * 2;
-        while i > 0 {
-            let index = (i - 1) % n;
+        let mut result = vec![-1; n];
+        let mut stack = VecDeque::new();
+        for i in 0..(2 * n - 1) {
+            let index = i % n;
             let num = nums[index];
-
-            let mut j = stack.len();
-            while j > 0 {
-                let top = stack[j - 1];
-                if top > num {
-                    ans_map.insert(index, top);
-                    break;
-                } else {
-                    stack.pop();
-                }
-                j -= 1;
+            while stack.len() > 0 && nums[*stack.front().unwrap()] < num {
+                let top = *stack.front().unwrap();
+                result[top] = num;
+                stack.pop_front();
             }
-            stack.push(num);
-            i -= 1;
+            stack.push_front(index);
         }
 
-        let mut result = vec![];
-        for i in 0..n {
-            if let Some(&ans) = ans_map.get(&i) {
-                result.push(ans);
-            } else {
-                result.push(-1);
-            }
-        }
         result
     }
 }
