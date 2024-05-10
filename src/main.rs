b46d15775791d0d78e4159c75bd125b027f63dd1
ShawnYuan <ysh83737@hotmail.com>
2024-05-10 18:02:39 +0800

    506. 相对名次 暴力解法 O(n^2)

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,30 @@
-use std::collections::HashMap;
-
 fn main() {
-    assert_eq!(Solution::find_words(vec!["Hello".to_string(),"Alaska".to_string(),"Dad".to_string(),"Peace".to_string()]), vec!["Alaska","Dad"]);
-    assert_eq!(Solution::find_words(vec!["omk".to_string()]), vec![] as Vec<String>);
-    assert_eq!(Solution::find_words(vec!["Aasdfghjkl".to_string(),"Qwertyuiop".to_string(),"zZxcvbnm".to_string()]), vec!["Aasdfghjkl","Qwertyuiop","zZxcvbnm"]);
+    assert_eq!(Solution::find_relative_ranks(vec![5,4,3,2,1]), vec!["Gold Medal","Silver Medal","Bronze Medal","4","5"]);
+    assert_eq!(Solution::find_relative_ranks(vec![10,3,8,9,4]), vec!["Gold Medal","5","Bronze Medal","Silver Medal","4"]);
+    assert_eq!(Solution::find_relative_ranks(vec![123123,11921,1,0,123]), vec!["Gold Medal","Silver Medal","4","5","Bronze Medal"]);
 }
 
 struct Solution;
 
 impl Solution {
-    pub fn find_words(words: Vec<String>) -> Vec<String> {
-        const KEYBOARD: [&str; 3] = [
-            "qwertyuiop",
-            "asdfghjkl",
-            "zxcvbnm"
-        ];
-        let mut letter_map = HashMap::new();
-        KEYBOARD.iter().enumerate().for_each(|(i, row)| {
-            for key in row.chars() {
-                letter_map.insert(key, i);
+    pub fn find_relative_ranks(mut score: Vec<i32>) -> Vec<String> {
+        let medals = ["Gold Medal", "Silver Medal", "Bronze Medal"];
+        let n = score.len();
+        let mut result = vec![String::new(); n];
+        for i in 0..n {
+            let max_index = Solution::find_max_index(&score);
+            result[max_index] = medals.get(i).unwrap_or(&(i + 1).to_string().as_str()).to_string();
+            score[max_index] = -1;
+        }
+        result
+    }
+    pub fn find_max_index(score: &Vec<i32>) -> usize {
+        let mut index = 0;
+        for i in 1..score.len() {
+            if score[i] > score[index] {
+                index = i;
             }
-        });
-
-        words.into_iter().filter(|word| {
-            let mut temp = None;
-            for key in word.to_lowercase().chars() {
-                let row_index = letter_map.get(&key);
-                if temp != None && row_index != temp {
-                    return false;
-                }
-                temp = row_index;
-            }
-            true
-        }).collect()
+        }
+        index
     }
 }
