80237872d419ff6e0c13263b850abd4ce194452c
ShawnYuan <ysh83737@hotmail.com>
2024-05-28 18:31:21 +0800

    566. 重塑矩阵 O(nm) O(1)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 fn main() {
-    assert_eq!(Solution::array_pair_sum(vec![1,4,3,2]), 4);
-    assert_eq!(Solution::array_pair_sum(vec![6,2,6,5,1,2]), 9);
+    assert_eq!(Solution::matrix_reshape(vec![vec![1,2],vec![3,4]], 1, 4), vec![[1,2,3,4]]);
+    assert_eq!(Solution::matrix_reshape(vec![vec![1,2],vec![3,4]], 2, 4), vec![[1,2],[3,4]]);
 }
 
 struct Solution;
 
 impl Solution {
-    pub fn array_pair_sum(mut nums: Vec<i32>) -> i32 {
-        nums.sort();
+    pub fn matrix_reshape(mat: Vec<Vec<i32>>, r: i32, c: i32) -> Vec<Vec<i32>> {
+        let m: usize = mat.len();
+        let n: usize = if let Some(row) = mat.first() {
+            row.len()
+        } else {
+            0
+        };
+        if m * n != (r * c) as usize {
+            return mat;
+        }
 
-        let mut result = 0;
-        let mut i = 0;
-        while i < nums.len() {
-            result += nums[i];
-            i += 2;
+        let result_c = c as usize;
+        let mut result = vec![vec![0; result_c]; r as usize];
+        let mut row_index = 0;
+        let mut col_index = 0;
+        for row in mat {
+            for num in row {
+                result[row_index][col_index] = num;
+                col_index += 1;
+                if col_index >= result_c {
+                    col_index -= result_c;
+                    row_index += 1;
+                }
+            }
         }
         result
     }
