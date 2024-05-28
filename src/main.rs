d5d3cbecaf4e721890197002e7062b7a722ea1cf
ShawnYuan <ysh83737@hotmail.com>
2024-05-28 17:48:12 +0800

    561. 数组拆分 排序 O(nlogn) O(logn)

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
 fn main() {
-    assert_eq!(Solution::find_relative_ranks(vec![5,4,3,2,1]), vec!["Gold Medal","Silver Medal","Bronze Medal","4","5"]);
-    assert_eq!(Solution::find_relative_ranks(vec![10,3,8,9,4]), vec!["Gold Medal","5","Bronze Medal","Silver Medal","4"]);
-    assert_eq!(Solution::find_relative_ranks(vec![123123,11921,1,0,123]), vec!["Gold Medal","Silver Medal","4","5","Bronze Medal"]);
+    assert_eq!(Solution::array_pair_sum(vec![1,4,3,2]), 4);
+    assert_eq!(Solution::array_pair_sum(vec![6,2,6,5,1,2]), 9);
 }
 
 struct Solution;
 
 impl Solution {
-    pub fn find_relative_ranks(score: Vec<i32>) -> Vec<String> {
-        let medals = ["Gold Medal", "Silver Medal", "Bronze Medal"];
-        let mut result = vec![String::new(); score.len()];
+    pub fn array_pair_sum(mut nums: Vec<i32>) -> i32 {
+        nums.sort();
 
-        let mut score_indeies: Vec<_> = score.iter().enumerate().collect();
-        score_indeies.sort_by(|a, b| b.1.cmp(a.1));
-
-        score_indeies.iter().enumerate().for_each(|(i, (index, _))| {
-            result[*index] = medals
-                .get(i)
-                .unwrap_or(&(i + 1).to_string().as_str())
-                .to_string();
-        });
+        let mut result = 0;
+        let mut i = 0;
+        while i < nums.len() {
+            result += nums[i];
+            i += 2;
+        }
         result
     }
 }
