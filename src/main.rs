90308cf797ce84a2eb7e54f064d7c0a6e022c9c0
ShawnYuan <ysh83737@hotmail.com>
2024-05-29 11:24:36 +0800

    575. 分糖果 HashSet O(n) O(n)

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,29 @@
+use std::collections::HashSet;
+
 fn main() {
-    assert_eq!(Solution::matrix_reshape(vec![vec![1,2],vec![3,4]], 1, 4), vec![[1,2,3,4]]);
-    assert_eq!(Solution::matrix_reshape(vec![vec![1,2],vec![3,4]], 2, 4), vec![[1,2],[3,4]]);
+    assert_eq!(Solution::distribute_candies(vec![1,1,2,2,3,3]), 3);
+    assert_eq!(Solution::distribute_candies(vec![1,1,2,3]), 2);
+    assert_eq!(Solution::distribute_candies(vec![6,6,6,6]), 1);
 }
 
 struct Solution;
 
 impl Solution {
-    pub fn matrix_reshape(mat: Vec<Vec<i32>>, r: i32, c: i32) -> Vec<Vec<i32>> {
-        let m: usize = mat.len();
-        let n: usize = if let Some(row) = mat.first() {
-            row.len()
-        } else {
-            0
-        };
-        if m * n != (r * c) as usize {
-            return mat;
-        }
+    pub fn distribute_candies(candy_type: Vec<i32>) -> i32 {
+        let n = candy_type.len();
+        let mut rest = n / 2;
 
-        let result_c = c as usize;
-        let mut result = vec![vec![0; result_c]; r as usize];
-        let mut row_index = 0;
-        let mut col_index = 0;
-        for row in mat {
-            for num in row {
-                result[row_index][col_index] = num;
-                col_index += 1;
-                if col_index >= result_c {
-                    col_index -= result_c;
-                    row_index += 1;
+        let mut eaten = HashSet::new();
+        for candy in candy_type {
+            if eaten.contains(&candy) == false {
+                eaten.insert(candy);
+                rest -= 1;
+                if rest == 0 {
+                    break;
                 }
             }
         }
-        result
+
+        (n / 2 - rest) as i32
     }
 }
