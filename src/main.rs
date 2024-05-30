72de8b2bdb310437fa1e4091c66a66727c7a4bbf
ShawnYuan <ysh83737@hotmail.com>
2024-05-30 17:46:38 +0800

    594. 最长和谐子序列 O(n) O(n)

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     assert_eq!(Solution::find_lhs(vec![1,2,3,4]), 2);
     assert_eq!(Solution::find_lhs(vec![1,1,1,1]), 0);
     assert_eq!(Solution::find_lhs(vec![1,3,5,7,9]), 0);
-    assert_eq!(Solution::find_lhs(vec![1,3,3,3,3,4]), 5);
+    assert_eq!(Solution::find_lhs(vec![1,3,3,3,3,4,4]), 6);
     assert_eq!(Solution::find_lhs(vec![1,2,2,1]), 4);
     assert_eq!(Solution::find_lhs(vec![1]), 0);
 }
@@ -11,22 +11,19 @@ fn main() {
 struct Solution;
 
 impl Solution {
-    pub fn find_lhs(mut nums: Vec<i32>) -> i32 {
-        nums.sort_by(|a, b| a.cmp(b));
+    pub fn find_lhs(nums: Vec<i32>) -> i32 {
+        let mut map = std::collections::HashMap::new();
+        nums.iter().for_each(|num| {
+            map.entry(num).and_modify(|e| *e += 1).or_insert(1);
+        });
 
-        let mut i = 0;
-        let mut j = 0;
         let mut result = 0;
-
-        while j < nums.len() {
-            while i < j && nums[j] - nums[i] > 1 {
-                i += 1;
+        nums.iter().for_each(|num| {
+            if let Some(&count) = map.get(&(num - 1)) {
+                result = result.max(count + map.get(num).unwrap());
             }
-            if nums[j] - nums[i] == 1 {
-                result = result.max(j - i + 1);
-            }
-            j += 1;
-        }
-        result as i32
+        });
+        
+        result
     }
 }
