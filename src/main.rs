8415998ad8f145668d5e8a8aed8acfa08ddd5e4c
ShawnYuan <ysh83737@hotmail.com>
2024-06-06 18:26:24 +0800

    605. 种花问题 数学推断计数 O(n) O(1)

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,34 @@ fn main() {
     assert_eq!(Solution::can_place_flowers(vec![0,0,0,0,1], 2), true);
     assert_eq!(Solution::can_place_flowers(vec![0,0,0,1,0,1,0,0], 2), true);
     assert_eq!(Solution::can_place_flowers(vec![0,0,0,0,1,0,1,0,0], 2), true);
+    assert_eq!(Solution::can_place_flowers(vec![1,0,0,0,0,1], 2), false);
+    assert_eq!(Solution::can_place_flowers(vec![0], 1), true);
+    assert_eq!(Solution::can_place_flowers(vec![1,0], 1), false);
 }
 
 struct Solution;
 
 impl Solution {
     pub fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
+        let m = flowerbed.len() as i32;
         let mut count = 0;
-        let mut index = 0;
-        for i in 0..flowerbed.len() {
-            let curr = flowerbed.get(i).unwrap();
-            if curr == &1 { continue; }
-
-            if index + 1 == i { continue; }
-            if i > 0 {
-                let prev = flowerbed.get(i - 1).unwrap_or(&0);
-                if prev == &1 { continue; }
+        let mut prev = -1;
+        for i in 0..m {
+            if flowerbed[i as usize] == 1 {
+                let gap = if prev < 0 {
+                    i
+                } else {
+                    i - prev - 2
+                };
+                count += gap / 2;
+                prev = i;
             }
-
-            let next = flowerbed.get(i + 1).unwrap_or(&0);
-            if next == &1 { continue; }
-            index = i;
-            count += 1;
         }
-        return count >= n;
+        if prev < 0 {
+            count += (m + 1) / 2;
+        } else {
+            count += (m - prev - 1) / 2;
+        }
+        count >= n
     }
 }
