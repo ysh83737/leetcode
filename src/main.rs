d9c619924e6a4362cb1c95cb0cdf30d8ce0e2097
ShawnYuan <ysh83737@hotmail.com>
2024-07-08 14:46:24 +0800

    643. 子数组最大平均数 I 转化为求子数组最大和 滑动窗口 O(n) O(1)

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,32 @@
 fn main() {
-    assert_eq!(Solution::maximum_product(vec![-1,-2,-3]), -6);
-    assert_eq!(Solution::maximum_product(vec![-1,-2,-3,0]), 0);
-    assert_eq!(Solution::maximum_product(vec![-1,-2,-3,1]), 6);
-    assert_eq!(Solution::maximum_product(vec![-1,-2,-3,0,1]), 6);
-    assert_eq!(Solution::maximum_product(vec![-1,-2,-3,0,1,2]), 12);
-    assert_eq!(Solution::maximum_product(vec![0,1,2]), 0);
-    assert_eq!(Solution::maximum_product(vec![-3,0,1,2]), 0);
-    assert_eq!(Solution::maximum_product(vec![1,2,3]), 6);
-    assert_eq!(Solution::maximum_product(vec![1,2,3,4]), 24);
-    assert_eq!(Solution::maximum_product(vec![0,1,2,3]), 6);
-    assert_eq!(Solution::maximum_product(vec![-3,0,1,2,3]), 6);
-    assert_eq!(Solution::maximum_product(vec![-3,-2,0,1,2,3]), 18);
+    assert_eq!(Solution::find_max_average(vec![1,12,-5,-6,50,3], 4), 12.75);
 
 }
 
 struct Solution;
 
-const MAX: i32 = 1000;
-const MIN: i32 = -1000;
-
 impl Solution {
-    pub fn maximum_product(nums: Vec<i32>) -> i32 {
-        let mut min1 = MAX;
-        let mut min2 = MAX;
-
-        let mut max1 = MIN;
-        let mut max2 = MIN;
-        let mut max3 = MIN;
-        
-        for num in nums {
-            if num < min1 {
-                (min1, min2) = (num, min1);
-            } else if num < min2 {
-                min2 = num;
+    pub fn find_max_average(nums: Vec<i32>, k: i32) -> f64 {
+        let max_sum = Solution::find_max_sum(nums, k);
+        max_sum as f64 / k as f64
+    }
+    pub fn find_max_sum(nums: Vec<i32>, k: i32) -> i32 {
+        let n = nums.len();
+        let mut i = 0;
+        let mut j = 0;
+        let mut sum = 0;
+        let mut max_sum = 0;
+        while j < n {
+            sum += nums[j];
+            if j < (k as usize) {
+                max_sum = sum;
+            } else {
+                sum -= nums[i];
+                max_sum = max_sum.max(sum);
+                i += 1;
             }
-
-            if num > max1 {
-                (max1, max2, max3) = (num, max1, max2);
-            } else if num > max2 {
-                (max2, max3) = (num, max2);
-            } else if num > max3 {
-                max3 = num;
-            }
+            j += 1;
         }
-
-        
-        let positive = max1 * max2 * max3;
-        positive.max(max1 * min1 * min2)
+        max_sum
     }
 }
