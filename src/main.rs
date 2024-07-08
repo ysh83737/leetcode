1560901b22986fb5dcbc8563a71e1261768c303f
ShawnYuan <ysh83737@hotmail.com>
2024-07-08 11:44:05 +0800

    628. 三个数的最大乘积 O(nlogn) O(logn)

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,55 @@
 fn main() {
-    assert_eq!(Solution::can_place_flowers(vec![1,0,0,0,1], 1), true);
-    assert_eq!(Solution::can_place_flowers(vec![1,0,0,0,1], 2), false);
-    assert_eq!(Solution::can_place_flowers(vec![0,0,0,0,1], 2), true);
-    assert_eq!(Solution::can_place_flowers(vec![0,0,0,1,0,1,0,0], 2), true);
-    assert_eq!(Solution::can_place_flowers(vec![0,0,0,0,1,0,1,0,0], 2), true);
-    assert_eq!(Solution::can_place_flowers(vec![1,0,0,0,0,1], 2), false);
-    assert_eq!(Solution::can_place_flowers(vec![0], 1), true);
-    assert_eq!(Solution::can_place_flowers(vec![1,0], 1), false);
+    assert_eq!(Solution::maximum_product(vec![-1,-2,-3]), -6);
+    assert_eq!(Solution::maximum_product(vec![-1,-2,-3,0]), 0);
+    assert_eq!(Solution::maximum_product(vec![-1,-2,-3,1]), 6);
+    assert_eq!(Solution::maximum_product(vec![-1,-2,-3,0,1]), 6);
+    assert_eq!(Solution::maximum_product(vec![-1,-2,-3,0,1,2]), 12);
+    assert_eq!(Solution::maximum_product(vec![0,1,2]), 0);
+    assert_eq!(Solution::maximum_product(vec![-3,0,1,2]), 0);
+    assert_eq!(Solution::maximum_product(vec![1,2,3]), 6);
+    assert_eq!(Solution::maximum_product(vec![1,2,3,4]), 24);
+    assert_eq!(Solution::maximum_product(vec![0,1,2,3]), 6);
+    assert_eq!(Solution::maximum_product(vec![-3,0,1,2,3]), 6);
+    assert_eq!(Solution::maximum_product(vec![-3,-2,0,1,2,3]), 18);
+
 }
 
 struct Solution;
 
 impl Solution {
-    pub fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
-        enum Prev { Nothing, Planted, Flowerbed }
-        let mut prev = Prev::Nothing;
-        let mut count = 0;
-        for item in flowerbed {
-            if item == 0 {
-                match prev {
-                    Prev::Nothing => {
-                        count += 1;
-                        prev = Prev::Planted;
-                    },
-                    _ => { prev = Prev::Nothing; }
-                }
+    pub fn maximum_product(nums: Vec<i32>) -> i32 {
+        let mut sorted_nums = nums;
+        sorted_nums.sort();
+
+        let mut positive_nums = vec![];
+        let mut negative_nums = vec![];
+
+        for num in sorted_nums {
+            if num > 0 {
+                positive_nums.push(num);
             } else {
-                match prev {
-                    Prev::Planted => count -= 1,
-                    _ => {}
-                }
-                prev = Prev::Flowerbed;
+                // 0
+                negative_nums.push(num);
             }
         }
-        count >= n
+
+        match positive_nums.len() {
+            0 => {
+                negative_nums.into_iter().rev().take(3).reduce(|acc, e| acc * e).unwrap()
+            },
+            1 => {
+                positive_nums[0] * negative_nums.into_iter().take(2).reduce(|acc, e| acc * e).unwrap()
+            },
+            2 => {
+                let last = positive_nums[positive_nums.len() - 1];
+                last * negative_nums.into_iter().take(2).reduce(|acc, e| acc * e).unwrap()
+            },
+            _ => {
+                let last = positive_nums[positive_nums.len() - 1];
+                let mut max = positive_nums.into_iter().rev().take(3).reduce(|acc, e| acc * e).unwrap();
+                max = max.max(last * negative_nums.into_iter().take(2).reduce(|acc, e| acc * e).unwrap_or(0));
+                max
+            }
+        }
     }
 }
