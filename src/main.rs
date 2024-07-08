6212e6026f51b55d848f8cfed7d825d54d75ecc1
ShawnYuan <ysh83737@hotmail.com>
2024-07-08 11:47:54 +0800

    628. 三个数的最大乘积 排序 O(nlogn) O(logn)

--- src/main.rs
+++ src/main.rs
@@ -17,39 +17,12 @@ fn main() {
 struct Solution;
 
 impl Solution {
-    pub fn maximum_product(nums: Vec<i32>) -> i32 {
-        let mut sorted_nums = nums;
-        sorted_nums.sort();
+    pub fn maximum_product(mut nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        nums.sort();
 
-        let mut positive_nums = vec![];
-        let mut negative_nums = vec![];
-
-        for num in sorted_nums {
-            if num > 0 {
-                positive_nums.push(num);
-            } else {
-                // 0
-                negative_nums.push(num);
-            }
-        }
-
-        match positive_nums.len() {
-            0 => {
-                negative_nums.into_iter().rev().take(3).reduce(|acc, e| acc * e).unwrap()
-            },
-            1 => {
-                positive_nums[0] * negative_nums.into_iter().take(2).reduce(|acc, e| acc * e).unwrap()
-            },
-            2 => {
-                let last = positive_nums[positive_nums.len() - 1];
-                last * negative_nums.into_iter().take(2).reduce(|acc, e| acc * e).unwrap()
-            },
-            _ => {
-                let last = positive_nums[positive_nums.len() - 1];
-                let mut max = positive_nums.into_iter().rev().take(3).reduce(|acc, e| acc * e).unwrap();
-                max = max.max(last * negative_nums.into_iter().take(2).reduce(|acc, e| acc * e).unwrap_or(0));
-                max
-            }
-        }
+        
+        let positive = nums[n - 1] * nums[n - 2] * nums[n - 3];
+        positive.max(nums[0] * nums[1] * nums[n- 1])
     }
 }
