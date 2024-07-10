3070c3a657df7f2b5f2ef5f2674a0ae32a057f39
ShawnYuan <ysh83737@hotmail.com>
2024-07-10 14:24:50 +0800

    67. 二进制求和 倒序相加取余、取模 O(n) O(1)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    // assert_eq!(Solution::add_binary(String::from("11"), String::from("1")), String::from("100"));
+    assert_eq!(Solution::add_binary(String::from("11"), String::from("1")), String::from("100"));
     assert_eq!(Solution::add_binary(String::from("1010"), String::from("1011")), String::from("10101"));
 }
 
@@ -7,34 +7,41 @@ struct Solution;
 
 impl Solution {
     pub fn add_binary(a: String, b: String) -> String {
+        let mut i = a.len();
+        let mut j = b.len();
+        let mut carry = 0;
         let mut result = String::new();
-        let mut next = '0';
-        let mut i = 0;
+        let mut unshift = |item: u32| {
+            result.insert_str(0, item.to_string().as_str());
+        };
         let mut chars_a = a.chars().rev();
         let mut chars_b = b.chars().rev();
-        while i < a.len() || i < b.len() {
-            let mut item_a = chars_a.nth(0).unwrap_or('0');
-            if next == '1' {
-                if item_a == '1' {
-                    item_a = '0';
-                } else {
-                    item_a = '1';
-                    next = '0';
-                }
-            }
-            let item_b = chars_b.nth(0).unwrap_or('0');
-            if item_a == item_b {
-                result.insert(0, '0');
-                if item_a == '1' {
-                    next = '1';
-                }
-            } else {
-                result.insert(0, '1');
-            }
-            i += 1;
+
+        while i > 0 && j > 0 {
+            let item_a = chars_a.next().unwrap().to_digit(10).unwrap();
+            let item_b = chars_b.next().unwrap().to_digit(10).unwrap();
+            let sum = carry + item_a + item_b;
+            carry = sum / 2;
+            unshift(sum % 2);
+            i -= 1;
+            j -= 1;
         }
-        if next == '1' {
-            result.insert(0, '1');
+        while i > 0 {
+            let item_a = chars_a.next().unwrap().to_digit(10).unwrap();
+            let sum = carry + item_a;
+            carry = sum / 2;
+            unshift(sum % 2);
+            i -= 1;
+        }
+        while j > 0 {
+            let item_b = chars_b.next().unwrap().to_digit(10).unwrap();
+            let sum = carry + item_b;
+            carry = sum / 2;
+            unshift(sum % 2);
+            j -= 1;
+        }
+        if carry > 0 {
+            unshift(carry);
         }
         result
     }
