4612de5356f54a0b7f0dcd562cf13ffdd7726f7f
ShawnYuan <ysh83737@hotmail.com>
2024-07-10 15:13:28 +0800

    190. 颠倒二进制位 从尾部逐位复制，左移至结果的对应位置 O(logn) O(1)

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,15 @@
 fn main() {
-    assert_eq!(Solution::add_binary(String::from("11"), String::from("1")), String::from("100"));
-    assert_eq!(Solution::add_binary(String::from("1010"), String::from("1011")), String::from("10101"));
+    assert_eq!(Solution::reverse_bits(0b00000010100101000001111010011100), 964176192);
 }
 
 struct Solution;
 
 impl Solution {
-    pub fn add_binary(a: String, b: String) -> String {
-        let mut i = a.len();
-        let mut j = b.len();
-        let mut carry = 0;
-        let mut result = String::new();
-        let mut unshift = |item: u32| {
-            result.insert_str(0, item.to_string().as_str());
-        };
-        let mut chars_a = a.chars().rev();
-        let mut chars_b = b.chars().rev();
-
-        while i > 0 && j > 0 {
-            let item_a = chars_a.next().unwrap().to_digit(10).unwrap();
-            let item_b = chars_b.next().unwrap().to_digit(10).unwrap();
-            let sum = carry + item_a + item_b;
-            carry = sum / 2;
-            unshift(sum % 2);
-            i -= 1;
-            j -= 1;
-        }
-        while i > 0 {
-            let item_a = chars_a.next().unwrap().to_digit(10).unwrap();
-            let sum = carry + item_a;
-            carry = sum / 2;
-            unshift(sum % 2);
-            i -= 1;
-        }
-        while j > 0 {
-            let item_b = chars_b.next().unwrap().to_digit(10).unwrap();
-            let sum = carry + item_b;
-            carry = sum / 2;
-            unshift(sum % 2);
-            j -= 1;
-        }
-        if carry > 0 {
-            unshift(carry);
+    pub fn reverse_bits(mut x: u32) -> u32 {
+        let mut result = 0;
+        for i in 0..32 {
+            result |= (x & 1) << (32 - i - 1);
+            x = x >> 1;
         }
         result
     }
