c153c6f42d31ac106b30ae699d18b36b988981cc
ShawnYuan <ysh83737@hotmail.com>
2024-07-10 17:19:53 +0800

    191. 位1的个数 右移检测末尾1 O(n) O(1)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
 fn main() {
-    assert_eq!(Solution::reverse_bits(0b00000010100101000001111010011100), 964176192);
+    assert_eq!(Solution::hamming_weight(11), 3);
 }
 
 struct Solution;
 
-const M1: u32 = 0b10101010101010101010101010101010;
-const M2: u32 = 0b11001100110011001100110011001100;
-const M4: u32 = 0b11110000111100001111000011110000;
-const M8: u32 = 0b11111111000000001111111100000000;
 impl Solution {
-    pub fn reverse_bits(mut x: u32) -> u32 {
-        x = M1 & x << 1 | (M1 & x) >> 1;
-        x = M2 & x << 2 | (M2 & x) >> 2;
-        x = M4 & x << 4 | (M4 & x) >> 4;
-        x = M8 & x << 8 | (M8 & x) >> 8;
-        x >> 16 | x << 16
+    pub fn hamming_weight(mut n: i32) -> i32 {
+        let mut result = 0;
+        while n > 0 {
+            result += n & 1;
+            n = n >> 1;
+        }
+        result
     }
 }
