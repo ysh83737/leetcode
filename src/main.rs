3a40633549b0c90c0e91d2f1d2b2e64c9a9c33b6
ShawnYuan <ysh83737@hotmail.com>
2024-07-10 17:23:45 +0800

    191. 位1的个数 n & (n−1)把最低位的1变为0 O(logn) O(1)

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ impl Solution {
     pub fn hamming_weight(mut n: i32) -> i32 {
         let mut result = 0;
         while n > 0 {
-            result += n & 1;
-            n = n >> 1;
+            n &= n - 1;
+            result += 1;
         }
         result
     }
