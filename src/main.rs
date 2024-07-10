251e10b78da71d1f4e6218438ce1df8d28053c39
ShawnYuan <ysh83737@hotmail.com>
2024-07-10 11:20:48 +0800

    67. 二进制求和 拆解字符 O(n) O(1)

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,40 @@
 fn main() {
-    assert_eq!(Solution::find_error_nums(vec![1,2,2,4]), vec![2,3]);
-    assert_eq!(Solution::find_error_nums(vec![1,1]), vec![1,2]);
-    assert_eq!(Solution::find_error_nums(vec![2,2]), vec![2,1]);
-    assert_eq!(Solution::find_error_nums(vec![3,2,2]), vec![2,1]);
-
+    // assert_eq!(Solution::add_binary(String::from("11"), String::from("1")), String::from("100"));
+    assert_eq!(Solution::add_binary(String::from("1010"), String::from("1011")), String::from("10101"));
 }
 
 struct Solution;
 
 impl Solution {
-    pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
-        let n = nums.len() as i32;
-
-        let mut xor = 0;
-        for i in 1..=n {
-            xor ^= i ^ nums[i as usize - 1];
-        }
-
-        let lowbit = xor & -xor;
-        let mut num1 = 0;
-        let mut num2 = 0;
-        let mut grouping = |num: i32| {
-            if num & lowbit == 0 {
-                num1 ^= num;
+    pub fn add_binary(a: String, b: String) -> String {
+        let mut result = String::new();
+        let mut next = '0';
+        let mut i = 0;
+        let mut chars_a = a.chars().rev();
+        let mut chars_b = b.chars().rev();
+        while i < a.len() || i < b.len() {
+            let mut item_a = chars_a.nth(0).unwrap_or('0');
+            if next == '1' {
+                if item_a == '1' {
+                    item_a = '0';
+                } else {
+                    item_a = '1';
+                    next = '0';
+                }
+            }
+            let item_b = chars_b.nth(0).unwrap_or('0');
+            if item_a == item_b {
+                result.insert(0, '0');
+                if item_a == '1' {
+                    next = '1';
+                }
             } else {
-                num2 ^= num;
+                result.insert(0, '1');
             }
-        };
-        for i in 1..=n {
-            grouping(i);
-            grouping(nums[i as usize - 1]);
+            i += 1;
         }
-
-        let mut result = vec![num1, num2];
-        for num in nums {
-            if num == num2 {
-                result.reverse();
-                break;
-            }
+        if next == '1' {
+            result.insert(0, '1');
         }
         result
     }
