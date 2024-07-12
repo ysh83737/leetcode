347695d6337a50bbe0ff92428368995fb7543d88
ShawnYuan <ysh83737@hotmail.com>
2024-07-12 11:18:55 +0800

    338. 比特位计数 最低设置位 O(n) O(1)

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ struct Solution;
 impl Solution {
     pub fn count_bits(n: i32) -> Vec<i32> {
         let mut result = vec![0; n as usize + 1];
-        for i in 1..=n {
-            result[i as usize] = result[i as usize >> 1] + (i & 1);
+        for i in 1..=(n as usize) {
+            result[i] = result[i & (i - 1)] + 1;
         }
         result
     }
