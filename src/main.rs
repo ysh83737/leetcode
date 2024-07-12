fb65974ba2db523f4a41a350fb067f9df5f29dde
ShawnYuan <ysh83737@hotmail.com>
2024-07-12 09:51:56 +0800

    231. 2 的幂 log计算，判断小数

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,14 @@ fn main() {
     assert_eq!(Solution::is_power_of_two(1), true);
     assert_eq!(Solution::is_power_of_two(16), true);
     assert_eq!(Solution::is_power_of_two(3), false);
+    assert_eq!(Solution::is_power_of_two(536870912), true);
 }
 
 struct Solution;
 impl Solution {
-    pub fn is_power_of_two(mut n: i32) -> bool {
-        let mut bit_1 = false;
-        while n > 0 {
-            if n & 1 > 0 {
-                if bit_1 {
-                    return false;
-                }
-                bit_1 = true;
-            }
-            n >>= 1;
-        }
-        bit_1
+    pub fn is_power_of_two(n: i32) -> bool {
+        let log_2 = (n as f64).log(2.0);
+        // f64精度误差
+        log_2 - log_2.floor() < 1e-14
     }
 }
