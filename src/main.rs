1dc6ac2e26545ee67db33a4689539ac871663cae
ShawnYuan <ysh83737@hotmail.com>
2024-07-12 10:47:52 +0800

    338. 比特位计数 循环 + 计数循环 O(nlogn) O(1)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 fn main() {
-    assert_eq!(Solution::is_power_of_two(0), false);
-    assert_eq!(Solution::is_power_of_two(1), true);
-    assert_eq!(Solution::is_power_of_two(16), true);
-    assert_eq!(Solution::is_power_of_two(3), false);
-    assert_eq!(Solution::is_power_of_two(536870912), true);
+    assert_eq!(Solution::count_bits(2), vec![0,1,1]);
+    assert_eq!(Solution::count_bits(5), vec![0,1,1,2,1,2]);
 }
 
 struct Solution;
-
-const BIG: i32 = 1 << 30;
 impl Solution {
-    pub fn is_power_of_two(n: i32) -> bool {
-        n > 0 && BIG % n == 0
+    pub fn count_bits(n: i32) -> Vec<i32> {
+        let mut result = vec![];
+        for i in 0..=n {
+            let mut num = i;
+            let mut count_of_bit = 0;
+            while num > 0 {
+                count_of_bit += 1;
+                num = num & (num - 1);
+            }
+            result.push(count_of_bit);
+        }
+        result
     }
 }
