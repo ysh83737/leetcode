4b3c0ec7585781dafd6f3a774562fc4d6f073598
ShawnYuan <ysh83737@hotmail.com>
2024-07-12 11:16:18 +0800

    338. 比特位计数 最低有效位 O(n) O(1)

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,8 @@ struct Solution;
 impl Solution {
     pub fn count_bits(n: i32) -> Vec<i32> {
         let mut result = vec![0; n as usize + 1];
-        let mut high_bit = 0;
         for i in 1..=n {
-            if i & (i - 1) == 0 {
-                high_bit = i;
-            }
-            result[i as usize] = result[(i - high_bit) as usize] + 1;
+            result[i as usize] = result[i as usize >> 1] + (i & 1);
         }
         result
     }
