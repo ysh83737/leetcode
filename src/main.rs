00a95336acd8ff7c0c6f8433c3d7908ce6bd53a6
ShawnYuan <ysh83737@hotmail.com>
2024-07-12 09:25:23 +0800

    222. 完全二叉树的节点个数 二分查找 + 位元算 O(log2n) O(1)

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,42 @@ fn main() {
         }
     )));
     assert_eq!(Solution::count_nodes(root), 6);
+    let root = Some(Rc::new(RefCell::new(
+        TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(
+                TreeNode {
+                    val: 2,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: Some(Rc::new(RefCell::new(TreeNode::new(8)))),
+                        right: Some(Rc::new(RefCell::new(TreeNode::new(9))))
+                    }))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 4,
+                        left: Some(Rc::new(RefCell::new(TreeNode::new(10)))),
+                        right: Some(Rc::new(RefCell::new(TreeNode::new(11))))
+                    }))),
+                }
+            ))),
+            right: Some(Rc::new(RefCell::new(
+                TreeNode {
+                    val: 3,
+                    left: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 6,
+                        left: Some(Rc::new(RefCell::new(TreeNode::new(12)))),
+                        right: Some(Rc::new(RefCell::new(TreeNode::new(13))))
+                    }))),
+                    right: Some(Rc::new(RefCell::new(TreeNode {
+                        val: 7,
+                        left: Some(Rc::new(RefCell::new(TreeNode::new(14)))),
+                        right: Some(Rc::new(RefCell::new(TreeNode::new(15))))
+                    }))),
+                }
+            )))
+        }
+    )));
+    assert_eq!(Solution::count_nodes(root), 15);
 }
 
 struct Solution;
@@ -45,19 +81,38 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn count_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        let mut result = 0;
-        if let Some(value) = root {
-            result += 1;
-            let node = value.borrow();
-            let left = node.left.clone();
-            let right = node.right.clone();
-            if left != None {
-                result += Solution::count_nodes(left);
-            }
-            if right != None {
-                result += Solution::count_nodes(right);
+        if root.is_none() {
+            return 0;
+        }
+        let mut level = -1;
+        let mut node = root.clone();
+        while let Some(value) = node {
+            level += 1;
+            node = value.borrow().left.clone();
+        }
+        let mut low = 1 << level;
+        let mut high = (1 << (level + 1)) - 1;
+        while low < high {
+            let mid = (high - low + 1) / 2 + low;
+            if Self::exists(root.clone(), level, mid) {
+                low = mid;
+            } else {
+                high = mid - 1;
             }
         }
-        result
+        low
+    }
+    fn exists(root: Option<Rc<RefCell<TreeNode>>>, level: i32, k: i32) -> bool {
+        let mut bits = 1 << (level - 1);
+        let mut node = root;
+        while node.is_some() && bits > 0 {
+            node = if bits & k > 0 {
+                node.unwrap().borrow().right.clone()
+            } else {
+                node.unwrap().borrow().left.clone()
+            };
+            bits >>= 1;
+        }
+        node.is_some()
     }
 }
