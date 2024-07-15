adae138e0da28a87687e083f52a5797cf226b7ee
ShawnYuan <ysh83737@hotmail.com>
2024-07-15 11:20:23 +0800

    389. 找不同 计算ASCII码之差 O(n) O(1)

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,15 @@ fn main() {
 }
 
 struct Solution;
-use std::collections::HashMap;
 impl Solution {
     pub fn find_the_difference(s: String, t: String) -> char {
-        let mut map: HashMap<char, u32> = HashMap::new();
-        for ch in s.chars() {
-            map.entry(ch).and_modify(|e| *e += 1).or_insert(1);
-        }
+        let mut codes_t = 0;
         for ch in t.chars() {
-            if let Some(count) = map.get_mut(&ch) {
-                if count == &0 {
-                    return ch;
-                }
-                *count -= 1;
-            } else {
-                return ch;
-            }
+            codes_t += ch as u32;
         }
-        unreachable!()
+        for ch in s.chars() {
+            codes_t -= ch as u32;
+        }
+        char::from_u32(codes_t).unwrap()
     }
 }
